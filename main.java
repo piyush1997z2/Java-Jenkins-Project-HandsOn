a5a260d0835f1a1f638d581b209524abc096d08d
Piyush Chakraborty <cpiyush563@gmail.com>
2023-10-29 22:57:03 +0530

    Initial Commit

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main{
     public static void main (String [] args) {
 
         System.out.println("This is a simple Java Program");
-        System.out.println("My name is Piyush Chakraborty");
+        System.out.println("My name is Piyush");
         System.out.println("Output from Java Program");
         System.out.println(new Date());
 
